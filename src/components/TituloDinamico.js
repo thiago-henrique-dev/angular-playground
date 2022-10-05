6c6169aa8eb8d8c4cbb8490152c6361bed6c5ce7
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-10-05 14:24:07 -0300

    Estilizando um Component

---
+++ src/components/TituloDinamico.js
@@ -0,0 +1,23 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({ mode: "open"});
+        //base do componente
+        const componentRoot = document.createElement('h1');
+        componentRoot.textContent = 'Thiago'
+        //estilizar o componente
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `
+        //enviar para o shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+
+customElements.define('titulo-dinamico', TituloDinamico )
