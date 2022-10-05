bc568f4263e868c8aff6e5cf374dbdb8f6ab9151
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-10-05 14:48:15 -0300

    Setando Propriedades de Classes

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -2,9 +2,28 @@ class CardNews extends HTMLElement {
     constructor(){
         super();
         const shadow = this.attachShadow({ mode: "open"});
-        shadow.innerHTML = "<h1>Hello World</h1>"
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+        const cardLeft = document.createElement("div");
+        componentRoot.setAttribute("class", "card__left");
+        const cardRight = document.createElement("div");
+        componentRoot.setAttribute("class", "card__left");
+
+        
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+        return componentRoot;
+    }
+
+    styles(){
 
     }
+
 }
 
 customElements.define("card-news", CardNews)
